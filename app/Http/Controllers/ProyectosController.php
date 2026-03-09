60f12407d139e881a593fee65455a52861edcdfc
Pablo Durán <pabloduranballesteros@gmail.com>
2026-03-09 10:52:50 +0100

    Creador de proyectos terminado

--- app/Http/Controllers/ProyectosController.php
+++ app/Http/Controllers/ProyectosController.php
@@ -11,4 +11,19 @@ use Illuminate\Support\Str;
 class ProyectosController extends Controller
 {
    
+
+    public function crearProyecto(Request $request)
+    {
+        try {
+            $proyecto = new Proyectos();
+            $proyecto->nombre = $request->input('nombre');
+            $proyecto->tipo = $request->input('tipo'); 
+            $proyecto->save();
+
+            return response()->json(['message' => 'Proyecto creado exitosamente', 'proyecto' => $proyecto], 201);
+        } catch (\Exception $e) {
+            Log::error('Error al crear proyecto: ' . $e->getMessage());
+            return response()->json(['message' => 'Error al crear proyecto'], 500);
+        }
+    }
 }
