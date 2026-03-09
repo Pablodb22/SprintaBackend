ae22fd01b6392726a8b63fcea59abe679e2e90d4
Pablo Durán <pabloduranballesteros@gmail.com>
2026-03-09 12:21:07 +0100

    Terminado y corredigo apartado de proyectos tanto crear como visualizar

--- app/Http/Controllers/ProyectosController.php
+++ app/Http/Controllers/ProyectosController.php
@@ -12,18 +12,44 @@ class ProyectosController extends Controller
 {
    
 
-    public function crearProyecto(Request $request)
-    {
-        try {
-            $proyecto = new Proyectos();
-            $proyecto->nombre = $request->input('nombre');
-            $proyecto->tipo = $request->input('tipo'); 
-            $proyecto->save();
+public function crearProyecto(Request $request)
+{
+    try {
+        $proyecto = new Proyectos();
+        $proyecto->nombre = $request->input('nombre');
+        $proyecto->tipo = $request->input('tipo');
+        $proyecto->descripcion = $request->input('descripcion'); 
+        $proyecto->empresa= $request->input('empresa');
+        $proyecto->save();
 
-            return response()->json(['message' => 'Proyecto creado exitosamente', 'proyecto' => $proyecto], 201);
-        } catch (\Exception $e) {
-            Log::error('Error al crear proyecto: ' . $e->getMessage());
-            return response()->json(['message' => 'Error al crear proyecto'], 500);
-        }
+        return response()->json(['message' => 'Proyecto creado exitosamente', 'proyecto' => $proyecto], 201);
+    } catch (\Exception $e) {
+        Log::error('Error al crear proyecto: ' . $e->getMessage());
+        return response()->json(['message' => 'Error al crear proyecto'], 500);
     }
 }
+
+public function getProyectos(Request $request)
+{
+    try {
+        $empresa = $request->query('empresa');
+        
+        if (!$empresa) {
+            return response()->json(['message' => 'Código de empresa requerido'], 400);
+        }
+
+        $proyectos = Proyectos::where('empresa', $empresa)->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $proyectos
+        ], 200);
+
+    } catch (\Exception $e) {
+        Log::error('Error al obtener proyectos: ' . $e->getMessage());
+        return response()->json(['message' => 'Error al obtener proyectos'], 500);
+    }
+}
+
+}
+
